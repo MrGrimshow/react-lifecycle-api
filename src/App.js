122f52bd7e44f617a86079a0a214fb985e201afa
Herbert <grimshowmaino@gmil.com>
2021-09-29 12:11:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {users: []};
+  }
+  async getUsers() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ users: users });
+      });
+  }
+  componentDidMount() {
+    this.getUsers();
+  }
+  render() {
+    return   <>
+    {this.state.users.map((user) => {
+      return <h1 key={user.id}>{user.name}<br/>{user.username}<br/>{user.email}<br/><br/></h1>;
+    })}
+  </>
+  }
 }
 
 export default App;
